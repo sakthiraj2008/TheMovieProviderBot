2a09bb0912ca11d4a4276d8cc0a440edc9fa110a
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2024-04-03 10:04:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,2 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git /TheMovieProviderBot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMovieProviderBot
-fi
-cd /TheMovieProviderBot
-pip3 install -U -r requirements.txt
-echo "Starting TheMovieProviderBot...."
+echo "Starting Bot.."
 python3 bot.py
